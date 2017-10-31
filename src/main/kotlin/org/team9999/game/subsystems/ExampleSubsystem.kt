ef305086a66b214d2bf198ec24261708e3fc77d2
Shmish <shmish90@gmail.com>
2017-10-30 23:18:16 -0400

    fixed dependancies

---
+++ src/main/kotlin/org/team9999/game/subsystems/ExampleSubsystem.kt
@@ -0,0 +1,9 @@
+package org.team9999.game.subsystems
+
+import edu.wpi.first.wpilibj.command.Subsystem
+
+class ExampleSubsystem : Subsystem () {
+  override fun initDefaultCommand () {
+    
+  }
+}
